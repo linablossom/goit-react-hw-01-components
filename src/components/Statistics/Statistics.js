396a7dba67f57a90bf21f20ba7c746ee57026027
Alina Akompi <alina.akompi@gmail.com>
2021-07-20 01:53:22 +0300

    add changes

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -17,9 +17,14 @@ const Statistics = ({ title, stats }) => (
 );
 
 Statistics.propTypes = {
-  id: PropTypes.string.isRequired,
-  label: PropTypes.string.isRequired,
-  percentage: PropTypes.number.isRequired,
+  title: PropTypes.string,
+  stats: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string,
+      label: PropTypes.string,
+      percentage: PropTypes.number,
+    })
+  ).isRequired,
 };
 
 export default Statistics;
